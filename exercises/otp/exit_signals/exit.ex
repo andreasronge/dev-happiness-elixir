f7849cb34be7212531f5545c1d06e4a9a0dc6392
Andreas Ronge <andreas.ronge@jayway.com>
2019-01-11 19:27:12 +0100

    OTP exit signals

---
+++ exercises/otp/exit_signals/exit.ex
@@ -0,0 +1,11 @@
+defmodule ExitDemo do
+  def loop() do
+    receive do
+      :hi -> loop()
+      :bye -> IO.puts("Bye")
+      :boom -> throw("Boom")
+    end
+  end
+end
+
+# pid = spawn_link(ExitDemo, :listen, [])
