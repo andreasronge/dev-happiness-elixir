5a5b18e3c4551ba0a6451d26245ca3b67a108e07
Andreas Ronge <andreas.ronge@jayway.com>
2019-01-15 18:06:30 +0100

    Removed old files

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 Some study notes while learning elixir
-
--- my_module.ex
+++
@@ -1,8 +0,0 @@
-defmodule MyModule do
-  def sum(x, y) do
-    x + y
-  end
-
-  # oneliner
-  def add_one(x), do: sum(x, 1)  # No dot needed
-end
